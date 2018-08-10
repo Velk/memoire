3a42368992dfd76eca94147dad0b574aeaf63884
GaelMrn <gael.etude@hotmail.com>
2018-08-10 23:20:34 +0200

    theming : Change the display of prices. Remove decimal digits.

--- sites/all/modules/memory_blocks/js/destination.js
+++ sites/all/modules/memory_blocks/js/destination.js
@@ -18,6 +18,17 @@
 
             $(".node-activite .node-links .node-readmore > a").text("PLUS D'INFORMATIONS");
 
+
+            /* Get price on cards (vignettes) */
+            $(".node-activite .node-content .field-name-field-price-prestation .field-item").each(function(){
+
+                // Get price and split to retrieve only the price and excluding prefix (€)
+                var priceVignette = $(this).text().split(' ')[0];
+                // Remove the two last decimal digits and also the dot separator
+                priceVignette = priceVignette.slice(0, -3);
+                // Change text of the price
+                $(this).text(priceVignette + " €");
+            });
         }
     };
 }(jQuery));
