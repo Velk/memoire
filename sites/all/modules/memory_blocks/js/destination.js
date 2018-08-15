88e8531123e3cccad158eb59e76e10e239063db3
GaelMrn <gael.etude@hotmail.com>
2018-08-16 00:32:55 +0200

    theming : Fix design for destination views.

--- sites/all/modules/memory_blocks/js/destination.js
+++ sites/all/modules/memory_blocks/js/destination.js
@@ -2,15 +2,15 @@
     Drupal.behaviors.memory_blocks_destination = {
         attach: function (context, settings) {
 
-            $(".block-views .views-col").mouseenter(function(){
+            $(".block-views .views-col .col-inner").mouseenter(function(){
 
                 $(".block-views .views-col .views-field-field-img-activite .field-content > img").css("filter", "grayscale()");
 
-                $(this).children(".col-inner").children(".views-field-field-img-activite").children(".field-content").children("img").css("filter", "none");
+                $(this).children(".views-field-field-img-activite").children(".field-content").children("img").css("filter", "none");
 
             });
 
-            $(".block-views .views-col").mouseleave(function(){
+            $(".block-views .views-col .col-inner").mouseleave(function(){
 
                 $(".block-views .views-col .views-field-field-img-activite .field-content > img").css("filter", "none");
 
