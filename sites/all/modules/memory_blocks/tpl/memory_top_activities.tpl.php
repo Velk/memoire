76f4b2e5368cab533ba73f8fd3c54fdbf2ef75a7
GaelMrn <gael.etude@hotmail.com>
2018-10-04 16:11:09 +0200

    theming : Add top activities and its manager in administration. Add WYSIWYG text format for the description of top destinations / activities.

---
+++ sites/all/modules/memory_blocks/tpl/memory_top_activities.tpl.php
@@ -0,0 +1,18 @@
+<div id="top-activities-container">
+    <h1><?php echo $title; ?></h1>
+    <div id="description"><?php echo $description; ?></div>
+    <div id="top-activities-grid">
+        <?php
+        for( $i = 0 ; $i < sizeof($arrayAllDatas) ; $i++ ){
+            ?>
+            <div>
+                <a href="<?php echo $arrayAllDatas[$i]["url"]; ?>">
+                    <p class="ta-name"><?php echo $arrayAllDatas[$i]["name"]; ?></p>
+                    <img src="<?php echo $arrayAllDatas[$i]["image"]; ?>" class="ta-img"/>
+                </a>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</div>
