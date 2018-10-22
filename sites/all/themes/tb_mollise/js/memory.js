00e7af62db4dac6c85d411c924e1af80c972f73f
GaelMrn <gael.etude@hotmail.com>
2018-10-22 19:29:22 +0200

    theming : Set menu to white color. Set back diminution when scrolling. Reduce font size.

--- sites/all/themes/tb_mollise/js/memory.js
+++ sites/all/themes/tb_mollise/js/memory.js
@@ -50,30 +50,30 @@
         });
 
         /* Scroll - Resize header menu */
-        // $( window ).scroll(function() {
-        //
-        //     if($(window).scrollTop() > 0){
-        //
-        //         $("#logo > img").css("height", "50px");
-        //         $("#menu-bar-wrapper").css("padding", "10px 0");
-        //         $("div#menu-bar-wrapper > div > div > div > ul > li > a").css("padding", "5px 30px");
-        //         $( "a:contains('Connexion')" ).parent("li").css("transform", "translateY(-13px)");
-        //         $("div#page").css("margin-top", "60px");
-        //         // $("div#page").css("margin-top", "calc(60px + 34px)");
-        //         $("div#block-memory-blocks-social-networks").css("top", "calc(60px + 230px)");
-        //     }
-        //     if($(window).scrollTop() === 0){
-        //
-        //         $("#logo > img").css("height", "70px");
-        //         $("#menu-bar-wrapper").css("padding", "15px 0");
-        //         $("div#menu-bar-wrapper > div > div > div > ul > li > a").css("padding", "10px 30px");
-        //         $( "a:contains('Connexion')" ).parent("li").css("transform", "translateY(-15px)");
-        //         $("div#page").css("margin-top", "80px");
-        //         // $("div#page").css("margin-top", "calc(80px + 34px)");
-        //         $("div#block-memory-blocks-social-networks").css("top", "calc(80px + 230px)");
-        //     }
-        //
-        // });
+        $( window ).scroll(function() {
+
+            if($(window).scrollTop() > 0){
+
+                $("#logo > img").css("height", "50px");
+                $("#menu-bar-wrapper").css("padding", "10px 0");
+                $("div#menu-bar-wrapper > div > div > div > ul > li > a").css("padding", "5px 30px");
+                $( "a:contains('Connexion')" ).parent("li").css("transform", "translateY(-13px)");
+                $("div#page").css("margin-top", "60px");
+                // $("div#page").css("margin-top", "calc(60px + 34px)");
+                $("div#block-memory-blocks-social-networks").css("top", "calc(60px + 230px)");
+            }
+            if($(window).scrollTop() === 0){
+
+                $("#logo > img").css("height", "70px");
+                $("#menu-bar-wrapper").css("padding", "15px 0");
+                $("div#menu-bar-wrapper > div > div > div > ul > li > a").css("padding", "10px 30px");
+                $( "a:contains('Connexion')" ).parent("li").css("transform", "translateY(-15px)");
+                $("div#page").css("margin-top", "80px");
+                // $("div#page").css("margin-top", "calc(80px + 34px)");
+                $("div#block-memory-blocks-social-networks").css("top", "calc(80px + 230px)");
+            }
+
+        });
 
     
 
