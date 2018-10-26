fa3af6aabcf236bd8b0f824b255307e4073974c6
GaelMrn <gael.etude@hotmail.com>
2018-10-27 00:51:58 +0200

    theming : Change video type - Add intern video. Add still image during the video loading and add sound control icon. Design scroll to the bottom of the slider button. Slider images, add previous and next icon. Add the possibility of adding link on an image.

--- sites/all/modules/memory_blocks/tpl/memory_slider.tpl.php
+++ sites/all/modules/memory_blocks/tpl/memory_slider.tpl.php
@@ -2,32 +2,55 @@
     <div>
         <?php
         if( $slider_type == 0 ){
-
-            for($i = 1 ; $i <= $array_size_slider ; $i++){
-
-                $slider_image_name = "slider_" . $i . "_name";
-                $slider_image_image = "slider_" . $i . "_image";
-
-                if( !empty($$slider_image_name) && !empty($$slider_image_image) ) {
         ?>
-                <div class="slider-img-container">
-                    <img src="<?php echo $$slider_image_image; ?>" alt="<?php echo $$slider_image_name; ?>" class="slider-img" />
-                </div>
-        <?php
-                }
-            }
+            <div id="slider-img-container">
+            <?php
+                for($i = 1 ; $i <= $array_size_slider ; $i++){
 
+                    $slider_image_name = "slider_" . $i . "_name";
+                    $slider_image_image = "slider_" . $i . "_image";
+                    $slider_image_redirection_link = "slider_" . $i . "_redirection_link";
+
+                    if( !empty($$slider_image_name) && !empty($$slider_image_image) ) {
+            ?>
+                    <div class="slider-img-container">
+                        <?php if( !empty($$slider_image_redirection_link) ){ ?>
+                        <a href="<?php echo $$slider_image_redirection_link; ?>">
+                        <?php } ?>
+                        <img src="<?php echo $$slider_image_image; ?>" alt="<?php echo $$slider_image_name; ?>" class="slider-img" />
+                        <?php if( !empty($$slider_image_redirection_link) ){ ?>
+                        </a>
+                        <?php } ?>
+                    </div>
+            <?php
+                    }
+                }
+            ?>
+            </div>
+            <div id="slider-actions">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+            </div>
+        <?php
         }
         ?>
         <?php
         if( $slider_type == 1 ){
         ?>
-            <iframe
-                    width="100%"
-                    height="100%"
-                    frameborder="0"
-                    src="<?php echo $video_url; ?>">
-            </iframe>
+            <video
+                width="100%"
+                height="100%"
+                <?php echo ($video_autoplay == 1) ? "autoplay" : ""; ?>
+                <?php echo ($video_controls == 1) ? "controls" : ""; ?>
+                <?php echo ($video_loop == 1) ? "loop" : ""; ?>
+                <?php echo ($video_muted == 1) ? "muted" : ""; ?>
+                <?php echo ($video_poster == 1 && !empty($video_poster_image) ) ? "poster=".$video_poster_image : ""; ?>
+            >
+                <source src="<?php echo $video_url_intern; ?>" type="video/mp4">
+            </video>
+            <div id="set-sound">
+                <i class="fa fa-volume-off" aria-hidden="true"></i>
+            </div>
         <?php
         }
         ?>
@@ -35,7 +58,13 @@
         if( $slider_type == 2 ){
         ?>
             <div id="still-img">
+                <?php if( !empty($still_img_redirection_link) ){ ?>
+                <a href="<?php echo $still_img_redirection_link; ?>">
+                <?php } ?>
                 <img src="<?php echo $still_img_image; ?>" alt="<?php echo $still_img_name; ?>" class="slider-img" />
+                <?php if( !empty($still_img_redirection_link) ){ ?>
+                </a>
+                <?php } ?>
             </div>
         <?php
         }
