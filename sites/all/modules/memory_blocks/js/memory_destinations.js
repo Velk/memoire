d37a438859ff09f9d6b55acd2a0f805b4d4a1429
GaelMrn <gael.etude@hotmail.com>
2018-11-01 18:46:29 +0100

    theming : 2nd version of the design of the template page Destination/ville. Add filters.

--- sites/all/modules/memory_blocks/js/memory_destinations.js
+++ sites/all/modules/memory_blocks/js/memory_destinations.js
@@ -2,6 +2,7 @@
 (function($) {
     $(document).ready(function($) {
 
+        /* Set the image on the entire size of its container depending on the container width greater than the image width or not */
         $(".cont-scop").each(function(){
 
             $(this).children("img.cont-vign-img").load(function(){
@@ -16,8 +17,36 @@
                         "height": "auto",
                     });
                 }
-
             });
+        });
+
+        /* Filters for destinations page */
+        $(".cont-filter").click(function(){
+           console.log("p : " + $(this).children("p").text());
+
+           var selectedSection = $(this).children("p").text();
+
+           if(
+               selectedSection === "Toutes nos Activités" ||
+               selectedSection === "Nos Packs"
+           ){
+
+               $("#cont-main .cont-container").css("display", "block");
+           }else{
+
+               $("#cont-main .cont-container").each(function(){
+
+                   console.log("h2 : " + $(this).children("h2").text());
+
+                   var section = $(this).children("h2").text();
+
+                   if( selectedSection === section ){
+                       $(this).css("display", "block");
+                   }else{
+                       $(this).css("display", "none");
+                   }
+               });
+           }
 
         });
 
