8880009750ac1fc4d8219a6e223eb2abfd78a624
GaelMrn <gael.etude@hotmail.com>
2018-11-21 00:26:42 +0100

    theming : Changes for the contact page Part 1.

--- sites/all/modules/memory_blocks/js/memory_contact.js
+++ sites/all/modules/memory_blocks/js/memory_contact.js
@@ -17,6 +17,62 @@
                 }
             });
 
+            /* Show Others field when Others option is selected */
+            $(".webform-component--contact-details--others").hide();
+
+            $("select#edit-submitted-contact-details-nature-request").click(function(){
+
+                if( $("select#edit-submitted-contact-details-nature-request").find(":selected").text() === "Autres" ){
+
+                    $(".webform-component--contact-details--others").slideDown("slow");
+                }else{
+
+                    $(".webform-component--contact-details--others").slideUp("slow");
+                }
+            });
+
+
+            function fieldsBehavior() {
+                if( $("select#edit-submitted-contact-details-nature-request").find(":selected").text() === "Demande de devis" ){
+
+                    if( !$("fieldset.webform-component--quote-request").is(":visible") ){
+
+                        $("fieldset.webform-component--quote-request").slideDown("slow");
+                    }
+
+                    if( $(".webform-component--contact-details--others").is(":visible") ){
+
+                        $(".webform-component--contact-details--others").slideUp("slow");
+                    }
+                }
+                if( $("select#edit-submitted-contact-details-nature-request").find(":selected").text() === "Autres" ){
+
+                    if( !$(".webform-component--contact-details--others").is(":visible") ){
+
+                        $(".webform-component--contact-details--others").slideDown("slow");
+                    }
+
+                    if( $("fieldset.webform-component--quote-request").is(":visible") ){
+
+                        $("fieldset.webform-component--quote-request").slideUp("slow");
+                    }
+                }
+                if(
+                    $("select#edit-submitted-contact-details-nature-request").find(":selected").text() !== "Demande de devis" &&
+                    $("select#edit-submitted-contact-details-nature-request").find(":selected").text() !== "Autres"
+                ){
+
+                    if( $("fieldset.webform-component--quote-request").is(":visible") ){
+
+                        $("fieldset.webform-component--quote-request").slideUp("slow");
+                    }
+                    if( $(".webform-component--contact-details--others").is(":visible") ){
+
+                        $(".webform-component--contact-details--others").slideUp("slow");
+                    }
+                }
+            }
+            setInterval(fieldsBehavior, 500);
 
         }
     };
