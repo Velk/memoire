9a4234ae34e8120539ed48906548f95edeff369a
GaelMrn <gael.etude@hotmail.com>
2019-12-31 01:50:34 +0100

    last_update : Final version 2 of the bloc reviews and the footer. Start to add some new colors.

--- sites/all/themes/tb_mollise/template.php
+++ sites/all/themes/tb_mollise/template.php
@@ -18,4 +18,17 @@ function tb_mollise_preprocess_html(&$vars) {
   );
 
   drupal_add_html_head($viewport, 'viewport');
+
+//  $path = $GLOBALS['base_url'] . '/' . path_to_theme() . '/js/memory.js';
+//  $selectivizr = array(
+//    '#tag' => 'script',
+////    '#attributes' => array(
+////      'src' => $path,
+////    ),
+////    '#prefix' => '<!--[if lte IE 8]>',
+////    '#suffix' => '</script><![endif]-->',
+//    '#value' => ''
+//  );
+//  drupal_add_html_head($selectivizr, 'selectivizr');
+
 }
